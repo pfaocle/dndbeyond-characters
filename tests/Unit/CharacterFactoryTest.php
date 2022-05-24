343c0ff4aaf9750b94f94fc9de1385ea68a5df45
Paul Byrne <code@pfaocle.co.uk>
2022-05-24 20:49:18 +0100

    More unit tests

---
+++ tests/Unit/CharacterFactoryTest.php
@@ -0,0 +1,63 @@
+<?php
+
+use Pfaocle\DndBeyondCharacters\Character;
+use Pfaocle\DndBeyondCharacters\CharacterFactory;
+
+beforeeach(function () {
+    $this->data = (object) [
+        'id' => 1234567,
+        'name' => 'Sir Didymus',
+        'race' => (object) ['fullName' => 'Knight of Yore'],
+        'classes' => [
+            (object) ['level' => 3, 'definition' => (object) ['name' => 'Cleric']],
+        ],
+        'stats' => [2 => (object) ['value' => 11]],
+        'baseHitPoints' => 25,
+        'removedHitPoints' => 0,
+        'campaign' => (object) ['name' => 'No campaign'],
+        'decorations' => (object) ['avatarUrl' => 'https://example.com/test.png'],
+    ];
+});
+
+test('valid character creation', function () {
+    $character = CharacterFactory::createFromJson($this->data);
+
+    $this->assertTrue($character instanceof Character);
+    $this->assertEquals($character->id(), 1234567);
+    $this->assertEquals($character->name(), 'Sir Didymus');
+    $this->assertEquals($character->race(), 'Knight of Yore');
+    $this->assertEquals($character->class(), 'Cleric');
+    $this->assertEquals($character->level(), 3);
+    $this->assertEquals($character->campaign(), 'No campaign');
+    $this->assertEquals($character->avatar(), 'https://example.com/test.png');
+});
+
+test('valid creation with empty avatar', function () {
+    $data = $this->data;
+    $data->decorations->avatarUrl = '';
+    $character = CharacterFactory::createFromJson($data);
+    $this->assertEquals($character->avatar(), 'default.png');
+});
+
+test('valid creation with multiple classes', function () {
+    $data = $this->data;
+    $data->classes = [
+        (object) ['level' => 3, 'definition' => (object) ['name' => 'Monk']],
+        (object) ['level' => 1, 'definition' => (object) ['name' => 'Bard']],
+    ];
+    $character = CharacterFactory::createFromJson($data);
+    $this->assertEquals($character->class(), 'Monk/Bard');
+    $this->assertEquals($character->level(), 4);
+});
+
+test('non-numeric id cannot be used', function () {
+    $data = $this->data;
+    $data->id = 'abcdef';
+    CharacterFactory::createFromJson($data);
+})->throws('TypeError');
+
+test('id must be present', function () {
+    $data = $this->data;
+    unset($data->id);
+    CharacterFactory::createFromJson($data);
+})->throws(Exception::class, 'No character ID found in JSON.');
--- tests/Unit/CharacterTest.php
+++ tests/Unit/CharacterTest.php
@@ -20,3 +20,32 @@ test('create a character object', function () {
     $this->assertEquals($this->character->level(), 3);
     $this->assertEquals($this->character->class(), 'Fighter/Bard');
 });
+
+test('valid current hit points', function ($base, $removed, $expected) {
+    $this->character->setHitPoints($base, $removed);
+    $this->assertEquals($this->character->currentHitPoints(), $expected);
+})->with([
+    [25, 0, 25],
+    [25, 10, 15],
+    [0, 0, 0],
+    [0, 10, -10],
+    [10000000000000, 0, 10000000000000],
+    [10000000000000, 10000000000000, 0],
+    [10000000000000, 9999999999999, 1],
+]);
+
+test('json array returns expected result', function () {
+    $this->character->setHitPoints(25, 0);
+    $ja = $this->character->jsonArray();
+
+    $this->assertIsArray($ja);
+    $this->assertEquals($ja, [
+        'id' => 1234567,
+        'name' => 'Sir Didymus',
+        'raceclass' => 'Knight of Yore Fighter/Bard',
+        'level' => 3,
+        'campaign' => 'No campaign',
+        'hp' => '25/25',
+        'avatar' => 'default.png',
+    ]);
+});
